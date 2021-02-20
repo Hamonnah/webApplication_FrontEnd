23c787a1bd53e93a7165f3fa789c73a882405d5b
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2021-02-20 20:43:42 +0100

    not working

--- src/main/java/com/example/demo/domain/OrderService.java
+++ src/main/java/com/example/demo/domain/OrderService.java
@@ -30,13 +30,13 @@ public class OrderService {
 
     private Set exampleData() {
         Set orders = new HashSet<>();
-        orders.add(new Order("Oris Dental Narvik", "Gayle Laakmann McDowell",  200977L, "Implant", "Atlantis ISUS, Co/Cr porselen", 3, OrderType.HASTER));
-        orders.add(new Order("Oris Dental Madla", "The MIT Press", 200345L, "Fixed denture", "Etsebro Co/Cr", 1,  OrderType.HASTER));
-        orders.add(new Order("Opitz Tannhelsesenter AS", "Michael Sipser", 200872L, "Additional", "Implantatmodell med gingivamaske", 1, OrderType.HASTER));
-        orders.add(new Order("Smil Tannregulering AS", "Silberschatz, Galvin, Gagne", 200764L, "Fixed denture","IPS e.max", 8, OrderType.HASTER));
-        orders.add(new Order("Brynjar Herland AS", "Alex Michaelides", 200542L, "Implant", "NobelProcera FCZ", 5, OrderType.NORMAL));
-        orders.add(new Order("Fjærvik Klinikken AS", "Margaret Atwood", 200816L, "Implant", "Zirkonzahn Prettau på Titan-base", 8, OrderType.HASTER));
-        orders.add(new Order("Ullevål Stadion Tannklinikk AS", "Renée Watson", 200761L, "Implant", "Kirurgisk stent/operasjonsguide", 1, OrderType.NORMAL));
+        orders.add(new Order("Oris Dental Narvik", "Gayle Laakmann McDowell",  "200977", OrderType.HASTER));
+        orders.add(new Order("Oris Dental Madla", "The MIT Press", "200977", OrderType.HASTER));
+        orders.add(new Order("Opitz Tannhelsesenter AS", "Michael Sipser", "200977",  OrderType.HASTER));
+        orders.add(new Order("Smil Tannregulering AS", "Silberschatz, Galvin, Gagne", "200977",  OrderType.HASTER));
+        orders.add(new Order("Brynjar Herland AS", "Alex Michaelides", "200977",  OrderType.NORMAL));
+        orders.add(new Order("Fjærvik Klinikken AS", "Margaret Atwood", "200977",  OrderType.HASTER));
+        orders.add(new Order("Ullevål Stadion Tannklinikk AS", "Renée Watson", "2222", OrderType.NORMAL));
         return orders;
     }
 
